4a1609bd889d0c4adf777dd41923e0d92f9f395a
KenoBuss <56350765+KenoBuss@users.noreply.github.com>
2021-12-02 22:28:12 +0100

    Add files via upload
    --- Day 2: Dive! ---
    --- Part One ---
    Now, you need to figure out how to pilot this thing.
    
    It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
    
    forward X increases the horizontal position by X units.
    down X increases the depth by X units.
    up X decreases the depth by X units.
    Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of what you might expect.
    
    The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's going. For example:
    
    forward 5
    down 5
    forward 8
    up 3
    down 8
    forward 2
    Your horizontal position and depth both start at 0. The steps above would then modify them as follows:
    
    forward 5 adds 5 to your horizontal position, a total of 5.
    down 5 adds 5 to your depth, resulting in a value of 5.
    forward 8 adds 8 to your horizontal position, a total of 13.
    up 3 decreases your depth by 3, resulting in a value of 2.
    down 8 adds 8 to your depth, resulting in a value of 10.
    forward 2 adds 2 to your horizontal position, a total of 15.
    After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
    
    Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
    
    Your puzzle answer was [ ].
    
    --- Part Two ---
    Based on your calculations, the planned course doesn't seem to make any sense. You find the submarine manual and discover that the process is actually slightly more complicated.
    
    In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0. The commands also mean something entirely different than you first thought:
    
    down X increases your aim by X units.
    up X decreases your aim by X units.
    forward X does two things:
    It increases your horizontal position by X units.
    It increases your depth by your aim multiplied by X.
    Again note that since you're on a submarine, down and up do the opposite of what you might expect: "down" means aiming in the positive direction.
    
    Now, the above example does something different:
    
    forward 5 adds 5 to your horizontal position, a total of 5. Because your aim is 0, your depth does not change.
    down 5 adds 5 to your aim, resulting in a value of 5.
    forward 8 adds 8 to your horizontal position, a total of 13. Because your aim is 5, your depth increases by 8*5=40.
    up 3 decreases your aim by 3, resulting in a value of 2.
    down 8 adds 8 to your aim, resulting in a value of 10.
    forward 2 adds 2 to your horizontal position, a total of 15. Because your aim is 10, your depth increases by 2*10=20 to a total of 60.
    After following these new instructions, you would have a horizontal position of 15 and a depth of 60. (Multiplying these produces 900.)
    
    Using this new interpretation of the commands, calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
    
    Your puzzle answer was [ ].

---
+++ Day2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Data;
+
+
+
+namespace CodeofAdvent
+{
+    public static class utils
+    {
+        public static DataTable FileRead(string filePath)
+        {
+            DataTable dt = new DataTable(); //create a DataTable
+
+            //add tow DataColumns to dt
+            dt.Columns.Add(new DataColumn("Direction"));
+            dt.Columns.Add(new DataColumn("Value"));
+            List<string> fileInput = File.ReadAllLines(filePath).ToList(); //Read all rows of the input file and save them in a list<string>
+
+            //for each element in the list add the value as int to dt
+            for (int i = 0; i < fileInput.Count(); i++)
+            {
+                DataRow dr = dt.NewRow();
+                dr["Direction"] = fileInput[i].ToString().Split(' ')[0];
+                dr["Value"] = Convert.ToInt32(fileInput[i].ToString().Split(' ')[1]);
+                dt.Rows.Add(dr);
+            }
+            return dt; // return dt;
+        }
+    }
+    public class DayOne
+    {
+        static void Main()
+        {
+            DataTable dt = utils.FileRead("../../../../InputFileDay2.txt"); //create a DataTable dt and use the filepath to the input file
+            puzzleOne(dt);
+            puzzleTow(dt);
+        }
+
+        public static void puzzleOne(DataTable dt)
+        {
+            //count all Values
+            int forward = count(dt, "forward");
+            int down = count(dt, "down");
+            int up = count(dt, "up");
+            int hight = down - up;
+
+            //calculate result
+            int result = forward * hight;
+            Console.WriteLine("The result of puzzle one is: " + result);
+        }
+
+        static int count(DataTable dt, string key)
+        {
+            int count = 0; //create a counter which contains the result of the puzzles
+
+            //loop throug each element of dt and check if direction == key
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (dt.Rows[i][0].ToString() == key)
+                {
+                    count += Convert.ToInt32(dt.Rows[i][1].ToString()); //Add Value to count
+                }
+            }
+            return count; //return count
+        }
+
+        public static void puzzleTow(DataTable dt)
+        {
+            int horizontal = 0;
+            int depth = 0;
+            int aim = 0;
+
+            //loop throug each element of dt and switch into the depending case
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                //save current value as X
+                int X = Convert.ToInt32(dt.Rows[i][1].ToString());
+
+                switch (dt.Rows[i][0].ToString())
+                {
+                    case "down":
+                        aim += X;
+                        break;
+
+                    case "up":
+                        aim -= X;
+                        break;
+
+                    case "forward":
+                        horizontal += X;
+                        if (aim != 0)
+                        {
+                            depth += (X * aim);
+                        }
+                        break;
+                }
+            }
+
+            int result = horizontal * depth;
+            Console.WriteLine("The result of puzzle tow is: " + result);
+        }
+    }
+}
